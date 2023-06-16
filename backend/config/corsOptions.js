47baef8bb1f38e7d57e4e10c6a65f652efa9f69b
Matthew9512 <mateuszmaciak95@gmail.com>
2023-06-16 22:19:42 +0200

    prod

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,16 +1,28 @@
-const whitelist = [
-   'https://mern-bank-account.netlify.app',
-   'http://127.0.0.1:5173',
-   'https://mern-bank-account.onrender.com',
-];
-const corsOptions = {
-   credentials: true,
-   optionsSuccessState: 200,
-
-   origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1) callback(null, true);
-      else callback(new Error('Not allowed by CORS'));
-   },
+const allowlist = ['http://example1.com', 'http://example2.com'];
+const corsOptionsDelegate = function (req, callback) {
+   let corsOptions;
+   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+   } else {
+      corsOptions = { origin: false }; // disable CORS for this request
+   }
+   callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
-module.exports = corsOptions;
+module.exports = corsOptionsDelegate;
+// const whitelist = [
+//    'https://mern-bank-account.netlify.app',
+//    'http://127.0.0.1:5173',
+//    'https://mern-bank-account.onrender.com',
+// ];
+// const corsOptions = {
+//    credentials: true,
+//    optionsSuccessState: 200,
+
+//    origin: (origin, callback) => {
+//       if (whitelist.indexOf(origin) !== -1) callback(null, true);
+//       else callback(new Error('Not allowed by CORS'));
+//    },
+// };
+
+// module.exports = corsOptions;
